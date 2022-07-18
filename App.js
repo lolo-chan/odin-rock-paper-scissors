fe36e8588ccc5848e0ec394545b8e4da7356642d
Rohan <rohanmchetan@gmail.com>
2022-07-18 10:41:19 +0530

    Fix check on getPlayerChoice

--- App.js
+++ App.js
@@ -5,24 +5,32 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-    let selection = prompt("Rock, Paper, or Scissors?");
-    playerchoice = selection.toLowerCase();
-    if(playerchoice==='rock' || playerchoice==='paper' || playerchoice==='scissors'){
-        return playerchoice.toLowerCase();
+    
+    let n = 0;
+    while(n==0){
+        let selection = prompt("Rock, Paper, or Scissors?");
+        playerchoice = selection.toLowerCase();
+        if(playerchoice==='rock' || playerchoice==='paper' || playerchoice==='scissors'){
+            return playerchoice.toLowerCase();
+            n=1;
+        }
+        else{
+            alert('Please choose a valid option');
+        }
     }
 }
 
 function gameRound(a,b) {
     if(a===b){
-        alert('Tie!');
+        alert(`Tie!\nComputers choice: ${a}\nYou chose: ${b}`);
     }
     else if((a==='rock' && b==='scissors') || (a==='paper' && b==='rock') || (a==='scissors' && b==='paper')){
-        alert('You lost!');
+        alert(`You lost!\nComputers choice: ${a}\nYou chose: ${b}`);
     }
     else{
-        alert('You won!');
+        alert(`You won!\nComputers choice: ${a}\nYou chose: ${b}`);
     }
 }
 
-
+gameRound(getComputerChoice(), getPlayerChoice());
 
