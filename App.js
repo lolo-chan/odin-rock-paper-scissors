12e262303500d647a1ab5df5d9b24e99193726d0
Rohan <rohanmchetan@gmail.com>
2022-07-18 09:26:46 +0530

    Change choice type from const to let

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
-    const randchoice = choices[Math.floor(Math.random()*3)];
+    let randchoice = choices[Math.floor(Math.random()*3)];
     return randchoice;
 }
 
 function getPlayerChoice() {
-    const playerchoice = prompt("Rock, Paper, or Scissors?");
+    let playerchoice = prompt("Rock, Paper, or Scissors?");
     return playerchoice;
 }
-
 
